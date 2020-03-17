514b105581321fd6829995a658d7d856e3ce9e6f
Niklas <wcvf@live.com>
2020-03-17 13:01:08 +0100

    Knap ikke færdig

---
+++ Knap.js
@@ -0,0 +1,17 @@
+var content = document.getElementById("content");
+var button = ducucment.getElementById("show-more");
+
+button.onclick = function (){
+    if (content.className == ...){
+
+
+    }
+
+    else{
+
+        
+    }
+
+}
+
+
