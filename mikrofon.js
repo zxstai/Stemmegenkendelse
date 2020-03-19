d05188a4479dd49af031da4f9e561fcefe0ae08c
Niklas <wcvf@live.com>
2020-03-19 14:30:59 +0100

    Forsøg på timer

--- mikrofon.js
+++ mikrofon.js
@@ -1,9 +1,11 @@
 const btn =document.querySelector('.talk')
 
+var timer;
 
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+recognition.lang = "da";
 
 recognition.onstart = function (){
  console.log('Stemmer lytter');
@@ -11,12 +13,34 @@ recognition.onstart = function (){
 }
  
 recognition.onresult = function(event){
-    conseole.log(event);
+    const ord = event.resultIndex;
+    const transcript = event.results[ord][0].transcript;
+    console.log(transcript);
 
 }
 
 btn.addEventListener('click', () =>{
     recognition.start();
+    if (timer < 0){
+        console.log("stopper med at lytte");
+    }
+    
+    setTimeout(btn.addEventListener()),3000);
+
+    recognition.onspeechend = function() {
+        recognition.stop();
+        console.log('Speech recognition has stopped.');
+      }
+      /*function nedtælling(){
+        timer = 5;
+        setInterval (nedtælling)(){
+            timer--;
+            if (counter === 0){
+
+            }
+        }
+
+      }*/
 });
 
 
