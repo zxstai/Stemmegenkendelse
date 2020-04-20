944fcf299270a6ab1d3ab12cd10ad94a110916fc
M. Hansen <xstaios123@gmail.com>
2020-04-20 11:30:57 +0200

    Frontpage

--- assets/js/findvej.js
+++ assets/js/findvej.js
@@ -5,17 +5,17 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='fwh
 anime.timeline({loop: true})
     .add({
         targets: '.fwhite',
-        translateX: [40,0],
+        translateX: [300,0],
         translateZ: 0,
         opacity: [0,1],
         easing: "easeOutExpo",
-        duration: 1200,
-        delay: (el, i) => 900 + 30 * i
+        duration: 5000,
+        delay: (el, i) => 500 + 30 * i
     }).add({
     targets: '.fwhite',
-    translateX: [0,-30],
+    translateX: [0,-850],
     opacity: [1,0],
     easing: "easeInExpo",
-    duration: 1100,
+    duration: 1200,
     delay: (el, i) => 100 + 30 * i
 });
